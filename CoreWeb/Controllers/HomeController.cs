b93b81a92c226a672a8fbf24fca1b5d25687d544
Clayton Allen <callen@utep.edu>
2019-02-20 10:20:26 -0700

    initial commit

---
+++ CoreWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace CoreWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string name1 = StandardDll.Class1.GetName();
+            string name2 = GetName();
+
+
+            return View();
+        }
+
+        public static string GetName()
+        {
+            SqlConnection conn = new SqlConnection("blank.  Not needed to demonstrate the problem");
+            DateTime d = conn.QuerySingle<DateTime>("select getDate()");
+
+
+            return d.ToLongDateString();
+        }
+    }
+}
---
+++ StandardDll/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace StandardDll
+{
+    public static class Class1
+    {
+        public static string GetName()
+        {
+            SqlConnection conn = new SqlConnection("blank.  Not needed to demonstrate the problem");
+            DateTime d = conn.QuerySingle<DateTime>("select getDate()");
+
+
+            return d.ToLongDateString();
+        }
+    }
+}
